c8a67d19aeb7447d6842af6411c404604f323206
Behindtheboard <jk2624@gmail.com>
2024-06-06 11:51:49 -0700

    Change gameboard spaces to be accessed via function

--- tictactoe-script.js
+++ tictactoe-script.js
@@ -1,23 +1,29 @@
 const Gameboard = (function () {
-    const gameBoard = [[], [], []];
-    const [rowOne, rowTwo, rowThree] = gameBoard;
-    const [r1c1, r1c2, r1c3] = rowOne;
-    const [r2c1, r2c2, r2c3] = rowTwo;
-    const [r3c1, r3c2, r3c3] = rowThree;
+    const gameBoardArray = [[], [], []];
+    const [rowOne, rowTwo, rowThree] = gameBoardArray;
 
-    return {r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3, rowOne}
+    const r1c1 = (xo) => rowOne[0] = xo;
+    const r1c2 = (xo) => rowOne[1] = xo;
+    const r1c3 = (xo) => rowOne[2] = xo;
+    const r2c1 = (xo) => rowTwo[0] = xo;
+    const r2c2 = (xo) => rowTwo[1] = xo;
+    const r2c3 = (xo) => rowTwo[2] = xo;
+    const r3c1 = (xo) => rowThree[0] = xo;
+    const r3c2 = (xo) => rowThree[1] = xo;
+    const r3c3 = (xo) => rowThree[2] = xo;
+
+    const displayBoard = () => gameBoardArray;
+
+    return {r1c1, r1c2, r1c3, 
+            r2c1, r2c2, r2c3, 
+            r3c1, r3c2, r3c3,
+            displayBoard}
 })();
 
+console.log(Gameboard.r1c1('x'))
+
 function player1(name) {
-    const boardMark = 'x';
-    
     return {name}
 }
 
-function player2(name) {
-    return {name}
-}
-
-Gameboard.r1c1 = "x";
-
-console.log(Gameboard.rowOne)
+console.log(Gameboard.gameBoardArray)
