369877fd5c813d12011f9b3c2db5917470b33029
Behindtheboard <jk2624@gmail.com>
2024-06-08 10:57:32 -0700

    Fix player select on board

--- tictactoe-script.js
+++ tictactoe-script.js
@@ -1,26 +1,20 @@
 const Gameboard = (function () {
-    const gameBoardArray = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']];
+    const gameBoardArray = [['r1c1','r1c2','r1c3'], ['r2c1','r2c2','r2c3'], ['r3c1','r3c2','r3c3']];
     const [rowOne, rowTwo, rowThree] = gameBoardArray;
-   
 
-    // const r1c1 = (xo) => rowOne[0] = xo;
-    // const r1c2 = (xo) => rowOne[1] = xo;
-    // const r1c3 = (xo) => rowOne[2] = xo;
-    // const r2c1 = (xo) => rowTwo[0] = xo;
-    // const r2c2 = (xo) => rowTwo[1] = xo;
-    // const r2c3 = (xo) => rowTwo[2] = xo;
-    // const r3c1 = (xo) => rowThree[0] = xo;
-    // const r3c2 = (xo) => rowThree[1] = xo;
-    // const r3c3 = (xo) => rowThree[2] = xo;
-
-    const pickSquare = (square, mark) => square = mark;
+    const pickSquare = function(square, mark) {
+        if (square.includes('r1') === true) {
+            rowOne[rowOne.indexOf(square)] = mark;
+        } else if (square.includes('r2') === true) {
+            rowTwo[rowTwo.indexOf(square)] =  mark;
+        } else if (square.includes('r3') === true) {
+            rowThree[rowThree.indexOf(square)] =  mark;
+        }
+    }
 
     const displayBoard = () => gameBoardArray;
 
-    return {r1c1, r1c2, r1c3, 
-            r2c1, r2c2, r2c3, 
-            r3c1, r3c2, r3c3,
-            displayBoard, pickSquare}
+    return {rowOne, rowTwo, rowThree, displayBoard, pickSquare}
 })();
 
 const winKey = (function() {
@@ -74,16 +68,14 @@ const player = function(name) {
 }
 
 const game = function() {
-    const board = Gameboard.displayBoard().join();
-    const boardDisplay = Gameboard.displayBoard();
+    const {rowOne, rowTwo, rowThree, pickSquare, displayBoard} = Gameboard;
+
+    const board = displayBoard().join();
     let playerX = "";
     let playerO = "";
 
-    const {r1c1, r1c2, r1c3, 
-        r2c1, r2c2, r2c3, 
-        r3c1, r3c2, r3c3} = Gameboard;
 
-    if (board === ' , , , , , , , , ') {
+    if (board === 'r1c1,r1c2,r1c3,r2c1,r2c2,r2c3,r3c1,r3c2,r3c3') {
         console.log("Let's play tic tac toe");
         if (playerX === "") {
             playerX = player(prompt("What's your name player X?"));
@@ -100,8 +92,10 @@ const game = function() {
         } 
         
         let square = prompt(`${playerX.name} Pick a Square. \n r1c1, r1c2, r1c3, \n r2c1, r2c2, r2c3, \n r3c1, r3c2, r3c3`)
-        Gameboard.pickSquare(square, playerX.getMark());
-        alert(boardDisplay)
+
+        alert(square)
+        pickSquare(square, playerX.getMark());
+        alert(displayBoard())
 
     } else if (board.includes(' ') === true) {
         console.log('Pick a Square' );
