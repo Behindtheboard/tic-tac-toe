8a9d1c04745aff81a05ec05e0841c0bed4699d37
Behindtheboard <jk2624@gmail.com>
2024-06-08 09:30:48 -0700

    Fix Player x or o selection logic

--- tictactoe-script.js
+++ tictactoe-script.js
@@ -1,23 +1,30 @@
 const Gameboard = (function () {
     const gameBoardArray = [['.','.','.'], ['.','.','.'], ['.','.','.']];
     const [rowOne, rowTwo, rowThree] = gameBoardArray;
+    const [r1c1, r1c2, r1c3] = rowOne;
+    const [r2c1, r2c2, r2c3] = rowTwo;
+    const [r3c1, r3c2, r3c3] = rowThree;
 
-    const r1c1 = (xo) => rowOne[0] = xo;
-    const r1c2 = (xo) => rowOne[1] = xo;
-    const r1c3 = (xo) => rowOne[2] = xo;
-    const r2c1 = (xo) => rowTwo[0] = xo;
-    const r2c2 = (xo) => rowTwo[1] = xo;
-    const r2c3 = (xo) => rowTwo[2] = xo;
-    const r3c1 = (xo) => rowThree[0] = xo;
-    const r3c2 = (xo) => rowThree[1] = xo;
-    const r3c3 = (xo) => rowThree[2] = xo;
+    // const r1c1 = (xo) => rowOne[0] = xo;
+    // const r1c2 = (xo) => rowOne[1] = xo;
+    // const r1c3 = (xo) => rowOne[2] = xo;
+    // const r2c1 = (xo) => rowTwo[0] = xo;
+    // const r2c2 = (xo) => rowTwo[1] = xo;
+    // const r2c3 = (xo) => rowTwo[2] = xo;
+    // const r3c1 = (xo) => rowThree[0] = xo;
+    // const r3c2 = (xo) => rowThree[1] = xo;
+    // const r3c3 = (xo) => rowThree[2] = xo;
+
+    const pickSquare = (square, mark) => square.splice(0, 1, mark);
+    pickSquare(r1c1, 'x');
+    console.log(gameBoardArray)
 
     const displayBoard = () => gameBoardArray;
 
     return {r1c1, r1c2, r1c3, 
             r2c1, r2c2, r2c3, 
             r3c1, r3c2, r3c3,
-            displayBoard}
+            displayBoard, pickSquare}
 })();
 
 const winKey = (function() {
@@ -67,31 +74,42 @@ const player = function(name) {
     const chooseMark = (pick) => mark = `${pick}`;
     const getMark = () => mark;
     
-    const pickSquare = (square) => {};
-    return {name, mark, getMark, chooseMark, pickSquare};
+    return {name, getMark, chooseMark};
 }
-
-const jason = player('jason');
-const hailey = player('hailey');
-console.log(jason);
-jason.chooseMark('x');
-console.log({mark: jason.getMark()});
-
-
-// const player2 = function(name) {
-//     (player1.mark1 === 'x') ? (mark2 = 'o') : (mark2 = 'x');
-//     return {name}
-// }
 
 const game = function() {
     const board = Gameboard.displayBoard().join();
+    const boardDisplay = Gameboard.displayBoard();
+    let playerX = "";
+    let playerO = "";
+
+    const {r1c1, r1c2, r1c3, 
+        r2c1, r2c2, r2c3, 
+        r3c1, r3c2, r3c3} = Gameboard;
 
     if (board === '.,.,.,.,.,.,.,.,.') {
-        console.log('Pick x or o');
+        console.log("Let's play tic tac toe");
+        if (playerX === "") {
+            playerX = player(prompt("What's your name player X?"));
+            alert(playerX.name);
+            playerX.chooseMark('x');
+            alert(playerX.getMark())
+        } 
+
+        if (playerO === "") {
+            playerO = player(prompt("What's your name player O?"));
+            alert(playerO.name);
+            playerO.chooseMark('o');
+            alert(playerO.getMark())
+        } 
         
+        let square = prompt(`${playerX.name} Pick a Square. \n r1c1, r1c2, r1c3, \n r2c1, r2c2, r2c3, \n r3c1, r3c2, r3c3`)
+        Gameboard.pickSquare(square, playerX.getMark());
+        alert(boardDisplay)
+
     } else if (board.includes('.') === true) {
-        console.log('Pick a Square');
-        console.log(Gameboard.displayBoard());
+        console.log('Pick a Square' );
+        console.log(boardDisplay);
     } else if (winKey.win(board) === true) {
         console.log("I win")
     } else {
